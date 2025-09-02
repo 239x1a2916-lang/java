2dcc5612937768d0d38cfc07a905a0a27cec68bf
239x1a2916-lang <239x1a2916@gprec.ac.in>
2025-09-02 16:25:05 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,44 @@
+package day2;
+
+public class student {
+String name;
+int num;
+String branch;
+	public static void main(String[] args) {
+		student s1 = new student();
+		s1.name = "cheery";
+		s1.num = 2916;
+		s1.branch = "cse";
+		System.out.println("the name of student is :"+s1.name);
+		System.out.println("the roll no :"+s1.num);
+		System.out.println("the branch is:"+s1.branch);
+		student s2 = new student();
+		s2.name = "keeru";
+		s2.num = 2922;
+		s2.branch = "cse";
+		System.out.println("the name of student is :"+s2.name);
+		System.out.println("the roll no :"+s2.num);
+		System.out.println("the branch is:"+s2.branch);
+		student s3 = new student();
+		s3.name = "varsha";
+		s3.num = 2908;
+		s3.branch = "cse";
+		System.out.println("the name of student is :"+s3.name);
+		System.out.println("the roll no :"+s3.num);
+		System.out.println("the branch is:"+s3.branch);
+		student s4 = new student();
+		s4.name = "uday";
+		s4.num = 2926;
+		s4.branch = "cse";
+		System.out.println("the name of student is :"+s4.name);
+		System.out.println("the roll no :"+s4.num);
+		System.out.println("the branch is:"+s4.branch);
+		student s5 = new student();
+		s5.name = "asii";
+		s5.num = 3201;
+		s5.branch = "csd";
+		System.out.println("the name of student is :"+s5.name);
+		System.out.println("the roll no :"+s5.num);
+		System.out.println("the branch is:"+s5.branch);
+	}
+}
