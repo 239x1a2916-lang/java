e9979ec2c25cc6322563c6a74261a27bd2bc2490
239x1a2916-lang <239x1a2916@gprec.ac.in>
2025-09-02 16:27:59 +0530

    Add files via upload

---
+++ conversion.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class conversion {
+	public static void main(String[] args) {
+int i = 88;
+double d = 23.7;
+
+System.out.println("i="+i);
+System.out.println("d="+d);
+System.out.println("After primitive data type conversion");
+d=i;
+System.out.println("i="+i);
+System.out.println("d="+d);
+	}
+
+}
